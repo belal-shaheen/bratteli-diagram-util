dc337370feacfde73f7b9c046e4204b813f5e135
Belal Shaheen <belal_shaheen@icloud.com>
2020-07-20 01:43:17 -0400

    styling

--- src/MatrixInput.js
+++ src/MatrixInput.js
@@ -2,10 +2,9 @@ import React from "react";
 import _ from "lodash";
 import mo from "./mo.png";
 import mi from "./mi.png";
-import './MatrixInput.css';
+import "./MatrixInput.css";
 
-import Collapsible from 'react-collapsible';
-
+import Collapsible from "react-collapsible";
 
 class MatrixInput extends React.Component {
   constructor(props) {
@@ -17,6 +16,8 @@ class MatrixInput extends React.Component {
       pushed: false,
       levels: [],
       edgeValues: [],
+      finalButtonDisabled: true,
+      submitButtonDisabled: false,
     };
   }
 
@@ -54,13 +55,11 @@ class MatrixInput extends React.Component {
     });
   }
 
-  async setEdgesString(levelCount, i, edgeString){
-
-    const clone = (this.state.edgeValues);
+  async setEdgesString(levelCount, i, edgeString) {
+    const clone = this.state.edgeValues;
 
     clone[levelCount][i] = edgeString;
     await this.setState({ edgeValues: clone });
-
   }
 
   async changeReloadState(x, y, levelCount) {
@@ -73,7 +72,7 @@ class MatrixInput extends React.Component {
       newEdgeArrays.push([]);
     }
 
-    this.setState({ levels: LevelrsArray , edgeValues: newEdgeArrays});
+    this.setState({ levels: LevelrsArray, edgeValues: newEdgeArrays });
   }
 
   Level = (levelCount) => {
@@ -82,12 +81,15 @@ class MatrixInput extends React.Component {
 
     return (
       <div>
+        <p class="level">{`Level ${levelCount+1}-${levelCount} incidence matrix: `}</p>
         <div>
           <label>
             X:
             <input
               type="number"
               name="x"
+              placeholder={`Vertices in level ${levelCount+1}`}
+              style={{ margin: "2px" }}
               onChange={(e) => {
                 const { name, value } = e.currentTarget;
                 x = value;
@@ -99,6 +101,9 @@ class MatrixInput extends React.Component {
             <input
               type="number"
               name="y"
+              placeholder={`Vertices in level ${levelCount}`}
+
+              style={{ margin: "2px" }}
               onChange={(e) => {
                 const { name, value } = e.currentTarget;
                 y = value;
@@ -119,8 +124,10 @@ class MatrixInput extends React.Component {
               {rows.map((col, j) => (
                 <div>
                   <input
+                    placeholder={`${i},${j}`} 
                     type="text"
                     value={col}
+                    style={{ margin: "2px" }}
                     onChange={(e) =>
                       this.setItem(
                         levelCount,
@@ -136,20 +143,23 @@ class MatrixInput extends React.Component {
           ))}
         </div>
         <div>
-          Edges
+          <p style={{ margin: "2px" }}>
+            Edges Values Separted by a comma(the fields will show up after you
+            set the dimensions):{" "}
+          </p>
           {this.state.reload[parseInt(levelCount)].map((rows, i) => (
-            <div> <div style={{ display: "flex" }}>
-                  <input
-                    type="text"
-                    onChange={(e) =>
-                      this.setEdgesString(
-                        levelCount,
-                        i,
-                        (e.currentTarget.value)
-                      )
-                    }
-                  />
-                </div>
+            <div>
+              {" "}
+              <div style={{ display: "flex" }}>
+                <input
+                  placeholder={`Vertex ${i}, Level ${levelCount+1}`}
+                  style={{ margin: "2px" }}
+                  type="text"
+                  onChange={(e) =>
+                    this.setEdgesString(levelCount, i, e.currentTarget.value)
+                  }
+                />
+              </div>
             </div>
           ))}
         </div>
@@ -158,6 +168,8 @@ class MatrixInput extends React.Component {
   };
 
   createLevels = () => {
+    this.setState({ finalButtonDisabled: false });
+    this.setState({ submitButtonDisabled: true });
     if (!this.state.pushed) {
       let temp = [];
       for (let i = 0; i < this.state.levelsCount; i++) {
@@ -178,40 +190,93 @@ class MatrixInput extends React.Component {
   passToParent = () => {
     const pass = this.props.setMatrixFromInput;
     pass(this.state.reload, this.state.levelsCount, this.state.edgeValues);
-  }
+  };
 
   render() {
     return (
       <div>
-        <Collapsible  trigger="Click on me for Instructions! ">
-        <div>
-        <label>The root level always has one node for now :)</label>
-          <ol>
-            <li>Set how many levels you want, start numbering from 1 and include the root with it. ( if we have the root and another level, we set the levels to 2. etc... )</li>
-            <li>After that set the dimensions of the incidence matrix for each level before filling any of them</li>
-            <li>Start filling the incidence matrix and the edge value fields</li>
-            <li>You can fill the edge value fields by providing a comma-seperated string, the program will input it from left to right.</li>
-          </ol>
-          <h5 style={{display:"inline"}}>Input</h5>
-          <img style={{width:"400px"}} src={mi}/>
-          <h5 style={{display:"inline"}}>Output:</h5>
-          <img style={{width:"200px"}} src={mo}/>
-          <p>If you have any problems with the input, refresh the site</p>
-        </div></Collapsible>
-        <br/>
-        <br/>
-        <br/>
-        
-        <br/>
-        <label>
-          Levels:
-          <input type="number" name="x" onChange={this.levelsOnChange}></input>
-          <button onClick={this.createLevels}>Submit</button>
-        </label>
-        {this.state.levels ? this.state.levels : <text>No Levels Yet</text>}
-        <button onClick={this.passToParent} >Submit To Graph</button>
+        <Collapsible trigger="----Click on me for Instructions!----">
+          <hr></hr>
+          <br />
+          <div className="instructions">
+            <label>The root level has one vertex for now &#128528;</label>
+            <div className="ins-list">
+              <ol>
+                <li>
+                  Set how many levels you want, start numbering from 1 and
+                  include the root with it. ( if we have the root and another
+                  level, we set the levels to 2. etc... )
+                </li>
+                <li>
+                  After that set the dimensions of the incidence matrix for each
+                  level before filling any of the actual matrices. (Note: click set before you move on to the next level)
+                </li>
+                <li>
+                  Start filling the incidence matrix and the edge value fields
+                </li>
+                <li>
+                  You can fill the edge value fields by providing a
+                  comma-seperated string, the program will input it from left to
+                  right for each vertex.
+                </li>
+                <li>
+                  Click the submit-graph button after you finish filling
+                  everything
+                </li>
+              </ol>
+            </div>
+            <h5 style={{ display: "inline", margin: "20px" }}> Input: </h5>
+            <img style={{ width: "400px", verticalAlign: "middle" }} src={mi} />
+            <h5 style={{ display: "inline", margin: "20px" }}> Output: </h5>
+            <img style={{ width: "200px", verticalAlign: "middle" }} src={mo} />
+            <p>If you have any problems with the input, refresh the site</p>
 
-        <footer id="footer">Email <a href="mailto: bshaheen@hamilton.edu">Belal</a> if you encounter any bugs!</footer>
+            <p>
+              Email{" "}
+              <a
+                style={{ color: "white" }}
+                href="mailto: bshaheen@hamilton.edu"
+              >
+                me
+              </a>{" "}
+              if you encounter any bugs!
+            </p>
+          </div>
+        </Collapsible>
+
+        <br />
+        <br />
+        <br />
+
+        <div className="levels">
+          <label>
+            Levels:
+            <input
+              style={{ margin: "2px" }}
+              type="number"
+              name="x"
+              onChange={this.levelsOnChange}
+            ></input>
+            <button
+              onClick={this.createLevels}
+              disabled={this.state.submitButtonDisabled}
+            style={{margin: "5px" }}
+
+            >
+              Submit
+            </button>
+          </label>
+          {this.state.levels ? this.state.levels : <text>No Levels Yet</text>}
+          <hr></hr>
+
+          <button
+            onClick={this.passToParent}
+            disabled={this.state.finalButtonDisabled}
+            style={{margin: "5px" }}
+          >
+            Submit To Graph
+          </button>
+        </div>
       </div>
     );
   }
